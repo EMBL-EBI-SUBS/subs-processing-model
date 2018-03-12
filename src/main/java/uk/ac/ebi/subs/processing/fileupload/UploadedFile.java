d3c6fad81a4eb9de9aa269858ba690859e1eb037
Karoly Erdos <karoly@ebi.ac.uk>
2018-03-12 10:43:06 +0000

    Remove unneeded fields

--- src/main/java/uk/ac/ebi/subs/processing/fileupload/UploadedFile.java
+++ src/main/java/uk/ac/ebi/subs/processing/fileupload/UploadedFile.java
@@ -5,25 +5,13 @@ import lombok.Data;
 @Data
 public class UploadedFile {
 
-    private String id;
-
-    private String generatedTusId;
-
     private String filename;
 
-    private String uploadPath;
-
-    private String targetPath;
+    private String path;
 
     private String submissionId;
 
     private long totalSize;
-
-    private long uploadedSize;
-
-    private String createdBy;
-
-    private FileStatus status;
 
     private String checksum;
 }
