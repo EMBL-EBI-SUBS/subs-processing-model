eefa14e6e04f89daf06e7158779e1b0a4528858f
Karoly Erdos <karoly@ebi.ac.uk>
2019-05-03 09:49:18 +0100

    Add JWT field to the ProcessingCertificateEnvelope container object

--- src/main/java/uk/ac/ebi/subs/processing/ProcessingCertificateEnvelope.java
+++ src/main/java/uk/ac/ebi/subs/processing/ProcessingCertificateEnvelope.java
@@ -1,6 +1,8 @@
 package uk.ac.ebi.subs.processing;
 
 
+import lombok.Builder;
+import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
@@ -8,37 +10,22 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-@ToString
-@EqualsAndHashCode
+@Data
+@Builder
 public class ProcessingCertificateEnvelope {
     String submissionId;
     List<ProcessingCertificate> processingCertificates = new ArrayList<>();
-
-    public ProcessingCertificateEnvelope(String submissionId) {
-        this.submissionId = submissionId;
-    }
-    public ProcessingCertificateEnvelope(String submissionId, List<ProcessingCertificate> processingCertificates) {
-        this.submissionId = submissionId;
-        this.processingCertificates = processingCertificates;
-    }
+    private String JWTToken;
 
     public ProcessingCertificateEnvelope() {
     }
 
-    public String getSubmissionId() {
-        return submissionId;
-    }
-
-    public void setSubmissionId(String submissionId) {
+    public ProcessingCertificateEnvelope(String submissionId) {
         this.submissionId = submissionId;
     }
 
-    public List<ProcessingCertificate> getProcessingCertificates() {
-        return processingCertificates;
-    }
-
-    public void setProcessingCertificates(List<ProcessingCertificate> processingCertificates) {
+    public ProcessingCertificateEnvelope(String submissionId, List<ProcessingCertificate> processingCertificates) {
+        this.submissionId = submissionId;
         this.processingCertificates = processingCertificates;
     }
-
 }
