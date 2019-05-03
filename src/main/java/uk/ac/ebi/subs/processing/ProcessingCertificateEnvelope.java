d74d255e18f2f9f18c465f6dfbd7e57950a9129f
Karoly Erdos <karoly@ebi.ac.uk>
2019-05-03 10:10:46 +0100

    Remove the builder annotation

--- src/main/java/uk/ac/ebi/subs/processing/ProcessingCertificateEnvelope.java
+++ src/main/java/uk/ac/ebi/subs/processing/ProcessingCertificateEnvelope.java
@@ -1,19 +1,15 @@
 package uk.ac.ebi.subs.processing;
 
 
-import lombok.Builder;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.ToString;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 @Data
-@Builder
 public class ProcessingCertificateEnvelope {
     String submissionId;
+
     List<ProcessingCertificate> processingCertificates = new ArrayList<>();
     private String JWTToken;
 
@@ -24,8 +20,10 @@ public class ProcessingCertificateEnvelope {
         this.submissionId = submissionId;
     }
 
-    public ProcessingCertificateEnvelope(String submissionId, List<ProcessingCertificate> processingCertificates) {
+    public ProcessingCertificateEnvelope(String submissionId, List<ProcessingCertificate> processingCertificates,
+                                         String jwtToken) {
         this.submissionId = submissionId;
         this.processingCertificates = processingCertificates;
+        this.JWTToken = jwtToken;
     }
 }
